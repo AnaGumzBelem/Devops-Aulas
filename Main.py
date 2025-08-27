00b2fd59e9417772296a30a8778171d3fd688cb1
AnaGumzBelem <anapraciturunen@gmail.com>
2025-08-27 00:20:29 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# MENSAGEM PRINCIPAL AO ACESSAR O IP DA APLICAÇÃO 127.0.0.1:8000
-@app.get("/")
+
+@app.get("/helloworld")
 async def root():
     return {"message": "Hello World"}
 
-# PARA ACESSAR ESTA MENSAGEM O ENDEREÇO É -> 127.0.0.1:8000/teste1
-@app.get("/teste1")
+
+@app.get("/funcaoteste")
 async def funcaoteste():
     return {"teste": "deu certo"}
 
